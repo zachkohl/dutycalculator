73f1a23abe0594dbe8ffc892a1cc6968e0e46e6a
zachkohl <zach.kohl@gmail.com>
2018-12-26 15:32:17 -0800

    dummy json data

--- dutycalculator.js
+++ dutycalculator.js
@@ -3,7 +3,8 @@
 
  function modelForExport(config) {
     app = config.app;
-
+    let cheerio = require('cheerio')
+  
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -19,9 +20,11 @@
  countryCode = req.body.countryCode;
  item = req.body.inputItem;
 
+ let $ = cheerio.load('https://usitc.gov/sites/default/files/tata/hts/hts_2018_revision_14_data.json')
 
-
-
+$.each(function(){
+ 
+})
 
         res.send('success:'+item)
      });//end '/home'
@@ -29,10 +32,42 @@
 
 
 
+    
+     var request = require('request')
+     , JSONStream = require('JSONStream')
+     , es = require('event-stream')
+    
+//    request({url: 'https://usitc.gov/sites/default/files/tata/hts/hts_2018_revision_14_data.json'})
+//      .pipe(JSONStream.parse('rows.*'))
+//      .pipe(es.mapSync(function (data) {
+//        console.error(data)
+//        console.log(data)
+//        return data
+//      }))
+
+
+request('https://usitc.gov/sites/default/files/tata/hts/hts_2018_revision_14_data.json').pipe(JSONStream.parse('other')).pipe(es.mapSync(function (data) {
+    console.error(data)
+    console.log(data)
+    return data
+  }))
 
 
 
 
+
+
+
+
+  app.get('/json', async function (req, res) {
+      JSONRaw = {
+          rows: '1',
+          name: 'zach',
+          sky: 'blue'
+      }
+jsonBlob = JSON.stringify(JSONRaw)
+    res.send(jsonBlob)
+ });//end '/home'
 
 
 

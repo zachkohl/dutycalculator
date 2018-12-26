7ead004e8f3dfa285ae66016564a65979d4c3c12
zachkohl <zach.kohl@gmail.com>
2018-12-26 13:27:56 -0800

    hello world working

--- config.js
+++ config.js
@@ -24,209 +24,209 @@ app.set('view engine', 'handlebars');
 //app.set('views', [path.join(__dirname,'views'),path.join(__dirname,'views','test')]);
 app.set('views','./views')
 app.use(express.static('static'));
-//app.listen(PORT, () => console.log('Example app listening on port 80!')) //see Socket.io below for new implementation
+app.listen(PORT, () => console.log('Example app listening on port 80!')) //see Socket.io below for new implementation
 
 
 config.app = app;
-//Database +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-var { Pool, Client } = require('pg')
+// //Database +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// var { Pool, Client } = require('pg')
 
-var client = new Client({
-connectionString: process.env.DATABASE_URL,
-ssl: true,
-});
+// var client = new Client({
+// connectionString: process.env.DATABASE_URL,
+// ssl: true,
+// });
 
-var pool = new Pool({
-connectionString: process.env.DATABASE_URL,
-ssl: true,
-});
-
-
-client.connect();
+// var pool = new Pool({
+// connectionString: process.env.DATABASE_URL,
+// ssl: true,
+// });
 
 
-function query(text, values,callback,errorLog) {
-  //Query
+// client.connect();
+
+
+// function query(text, values,callback,errorLog) {
+//   //Query
 
  
-  pool.query(text, values, (err, response) => {
+//   pool.query(text, values, (err, response) => {
     
-    if (err) {
-      // console.log(JSON.stringify(err.stack));
-console.log(err.stack.split('\n',1)[0])
-    } 
-    else {
-      if (response.rows) {
-        callback(response); //must use var because we want function scope. 
-        return response;
-      } 
-      else {
-        console.log('no rows')
-        errorLog('no rows');
-      };
+//     if (err) {
+//       // console.log(JSON.stringify(err.stack));
+// console.log(err.stack.split('\n',1)[0])
+//     } 
+//     else {
+//       if (response.rows) {
+//         callback(response); //must use var because we want function scope. 
+//         return response;
+//       } 
+//       else {
+//         console.log('no rows')
+//         errorLog('no rows');
+//       };
       
-      };//end else
+//       };//end else
 
-  })//end pool.query
+//   })//end pool.query
   
-};//end query
+// };//end query
 
 
-async function queryP(text, values,errorLog) {
-  //Query
-  const client = await pool.connect()
-  let response;
-  try{
-    response = await client.query(text,values)
-          if (response.rows) {
-        return response;
-      } 
-      else {
-        console.log('no rows')
-        errorLog('no rows');
-      };
-  }
-  catch(e){
-    console.log(e.stack)
-  }
-finally{
-  client.release();
-}
-  // (async () => {
-  //   const client = await pool.connect()
-  //   try {
-  //     const res = await client.query('SELECT * FROM users WHERE users_id = $1', [1])
-  //     //console.log(res.rows[0])
-  //     return new Promise(resolve => {resolve(res)},1000);
-  //   } finally {
-  //     client.release()
+// async function queryP(text, values,errorLog) {
+//   //Query
+//   const client = await pool.connect()
+//   let response;
+//   try{
+//     response = await client.query(text,values)
+//           if (response.rows) {
+//         return response;
+//       } 
+//       else {
+//         console.log('no rows')
+//         errorLog('no rows');
+//       };
+//   }
+//   catch(e){
+//     console.log(e.stack)
+//   }
+// finally{
+//   client.release();
+// }
+//   // (async () => {
+//   //   const client = await pool.connect()
+//   //   try {
+//   //     const res = await client.query('SELECT * FROM users WHERE users_id = $1', [1])
+//   //     //console.log(res.rows[0])
+//   //     return new Promise(resolve => {resolve(res)},1000);
+//   //   } finally {
+//   //     client.release()
     
-  //   }
-  // })().catch(e => console.log(e.stack))
+//   //   }
+//   // })().catch(e => console.log(e.stack))
   
-};//end queryP
+// };//end queryP
 
-// let text = 'INSERT INTO adult (adult_name) VALUES ($1) ON CONFLICT (adult_name) DO UPDATE SET adult_name = $1 RETURNING *'
-// let values = [adultNames[i][1]];
-// query(text, values, callback);
-// function callback(data) {
-// //data.rows
-//         }//end callback definition
+// // let text = 'INSERT INTO adult (adult_name) VALUES ($1) ON CONFLICT (adult_name) DO UPDATE SET adult_name = $1 RETURNING *'
+// // let values = [adultNames[i][1]];
+// // query(text, values, callback);
+// // function callback(data) {
+// // //data.rows
+// //         }//end callback definition
 
-config.query = query;
-config.queryP = queryP;
-config.pool = pool;
-config.client = client;
-//Session +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-const session = require('express-session');
-pgSession = require('connect-pg-simple')(session); //This all ends up encrypted on the client side. Used wireshark to check. 
+// config.query = query;
+// config.queryP = queryP;
+// config.pool = pool;
+// config.client = client;
+// //Session +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const session = require('express-session');
+// pgSession = require('connect-pg-simple')(session); //This all ends up encrypted on the client side. Used wireshark to check. 
 
-var sess = {
-  store: new pgSession({
-    pool : pool,                // Connection pool
-     // Use another table-name than the default "session" one
-  }),
-  secret: process.env.COOKIE_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true
-}
+// var sess = {
+//   store: new pgSession({
+//     pool : pool,                // Connection pool
+//      // Use another table-name than the default "session" one
+//   }),
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true
+// }
  
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
-}
+// if (app.get('env') === 'production') {
+//   app.set('trust proxy', 1) // trust first proxy
+//   sess.cookie.secure = true // serve secure cookies
+// }
  
-app.use(session(sess))
+// app.use(session(sess))
 
-//connect-flash
-let cookieParser = require('cookie-parser')
-let flash = require('connect-flash');
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(flash());
+// //connect-flash
+// let cookieParser = require('cookie-parser')
+// let flash = require('connect-flash');
+// app.use(cookieParser(process.env.COOKIE_SECRET))
+// app.use(flash());
 
 
-//Session stuff
-//https://www.npmjs.com/package/express-session
-//https://www.npmjs.com/package/connect-pg-simple
-//https://www.tutorialspoint.com/expressjs/expressjs_authentication.htm (seems very good)
+// //Session stuff
+// //https://www.npmjs.com/package/express-session
+// //https://www.npmjs.com/package/connect-pg-simple
+// //https://www.tutorialspoint.com/expressjs/expressjs_authentication.htm (seems very good)
 
-function checkSession(req, res, next) {
-  if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
-    res.locals.layout = "loggedin";
-    next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
-  } else {
-    res.redirect('login');
-    process.stdout.write('not logged in');
+// function checkSession(req, res, next) {
+//   if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
+//     res.locals.layout = "loggedin";
+//     next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
+//   } else {
+//     res.redirect('login');
+//     process.stdout.write('not logged in');
 
-  }
-};
-function requireBasicPlus(req, res, next) {
-  if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
-    res.locals.layout = "loggedin";
-    next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
-  } else {
-    res.redirect('login');
-    process.stdout.write('not logged in');
+//   }
+// };
+// function requireBasicPlus(req, res, next) {
+//   if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
+//     res.locals.layout = "loggedin";
+//     next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
+//   } else {
+//     res.redirect('login');
+//     process.stdout.write('not logged in');
 
-  }
-};
-function requireMiddlePlus(req, res, next) {
-  if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
-    if(req.session.role === 'admin' || req.session.role ==='Debbie' || req.session.role === 'Raja'){
-      res.locals.layout = "loggedin";
-      next(); 
-    }else{
-      res.send('Your account does not have premission to view this page, please contact your administrator for asstance with this feature');
-      return;
-    }
+//   }
+// };
+// function requireMiddlePlus(req, res, next) {
+//   if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
+//     if(req.session.role === 'admin' || req.session.role ==='Debbie' || req.session.role === 'Raja'){
+//       res.locals.layout = "loggedin";
+//       next(); 
+//     }else{
+//       res.send('Your account does not have premission to view this page, please contact your administrator for asstance with this feature');
+//       return;
+//     }
   
-  } else {
-    res.redirect('login');
-    process.stdout.write('not logged in');
+//   } else {
+//     res.redirect('login');
+//     process.stdout.write('not logged in');
 
-  }
-};
-function requireAdminPlus(req, res, next) {
-  if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
-    if(req.session.role === 'admin'){
-      res.locals.layout = "loggedin";
-      next(); 
-    }else{
-      res.send('Your account does not have premission to view this page, please contact your administrator for asstance with this feature');
-      return;
-    }
+//   }
+// };
+// function requireAdminPlus(req, res, next) {
+//   if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
+//     if(req.session.role === 'admin'){
+//       res.locals.layout = "loggedin";
+//       next(); 
+//     }else{
+//       res.send('Your account does not have premission to view this page, please contact your administrator for asstance with this feature');
+//       return;
+//     }
   
-  } else {
-    res.redirect('login');
-    process.stdout.write('not logged in');
+//   } else {
+//     res.redirect('login');
+//     process.stdout.write('not logged in');
 
-  }
-};
+//   }
+// };
 
-function checkSessionNolayoutChange(req, res, next) {
-  if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
+// function checkSessionNolayoutChange(req, res, next) {
+//   if (req.session.user){ //Does the session exist? Recall that the express-session is using its own id system in the background to hook into a specific user profile. 
 
-    next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
-  } else {
-    res.redirect('login');
-    process.stdout.write('not logged in');
+//     next(); //https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express Basically next is a special object in express that passes control to the next MATCHING route
+//   } else {
+//     res.redirect('login');
+//     process.stdout.write('not logged in');
 
-  }
-};
+//   }
+// };
 
-config.checkSession = checkSession;
-config.checkSessionNolayoutChange = checkSessionNolayoutChange;
-config.requireBasicPlus =requireBasicPlus;
-config.requireMiddlePlus =requireMiddlePlus;
-config.requireAdminPlus =requireAdminPlus;
+// config.checkSession = checkSession;
+// config.checkSessionNolayoutChange = checkSessionNolayoutChange;
+// config.requireBasicPlus =requireBasicPlus;
+// config.requireMiddlePlus =requireMiddlePlus;
+// config.requireAdminPlus =requireAdminPlus;
 
-//Socket.io++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-server.listen(PORT,console.log('listening on port '+PORT));
+// //Socket.io++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+// server.listen(PORT,console.log('listening on port '+PORT));
 
-config.io = io;
+// config.io = io;
 
 //https://socket.io/docs/#Installing
 
@@ -242,10 +242,10 @@ config.io = io;
 
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-config.sgMail = sgMail;
+// config.sgMail = sgMail;
 
 
 
--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 config = require('./config');
 
-config.app.get('/',config.checkSession, function (req, res) {
-   switch(req.session.role){
-case 'student':
-res.redirect('quiz');
-break;
-case 'admin':
-res.render('landing')
-break;
-case 'Debbie':
-res.redirect('answers');
-break;
-case 'Raja':
-res.redirect('answers');
-break;
-default:
-res.redirect('login')
-}
-
+config.app.get('/', function (req, res) {
+res.send('Hello John!')
 
 
 });//end '/'

2b88151d99865ad8f4b4136ddc104e8efa580358
zachkohl <zach.kohl@gmail.com>
2019-01-05 12:40:33 -0800

    fixed port

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(80, function () {
+app.listen(PORT, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
